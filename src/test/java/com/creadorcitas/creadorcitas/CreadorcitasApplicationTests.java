c7a2ccacdb5d57ea00233470abd3b0739e14a4dd
unknown <cr1st14n1998@hotmail.com>
2023-07-21 14:15:58 -0500

    Initial commit

---
+++ src/test/java/com/creadorcitas/creadorcitas/CreadorcitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creadorcitas.creadorcitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreadorcitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
